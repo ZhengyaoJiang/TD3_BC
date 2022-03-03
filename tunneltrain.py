0ecaa6516c5f43f6183be4b2c280c995b892f52a
Zhengyao Jiang <jzyjiangzhengyao@gmail.com>
2022-03-03 11:27:11 +0000

    pytorch computation

--- tunneltrain.py
+++ tunneltrain.py
@@ -6,8 +6,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import pandas as pd
+from typing import List
 import numba
 
+
+@torch.no_grad()
 def divide2trajectories(data):
     length = data["terminals"].shape[0]
     rewards = []
@@ -26,10 +30,12 @@ def divide2trajectories(data):
 def dict_to_tensor(inputs, device):
     return {k: torch.tensor(v, device=device, dtype=torch.float32) for k, v in inputs.items()}
 
-
-def get_returns(starts, ends, rewards):
-    returns = [rewards[start: end].sum() for start, end in zip(starts, ends)]
-    return np.array(returns)
+@torch.no_grad()
+@torch.jit.script
+def get_returns(starts: torch.Tensor, ends: torch.Tensor, rewards: torch.Tensor, discount: float):
+    returns = [(rewards[start: end]*(torch.cumprod(torch.ones(end-start, device=rewards.device)*discount, dim=0))/discount).sum()
+               for start, end in zip(starts, ends)]
+    return torch.stack(returns)
 
 @numba.jit(nopython=True, parallel=True)
 def get_return(start, end, rewards):
@@ -72,30 +78,33 @@ class TunnelDataSet():
         self.data = trajectory_data  # dictionary of lists of observations actions rewards and terminals
         self.discount = discount
 
-        self.test_data = dict(observations=[], actions=[], rewards=[], starts=[], ends=[], returns=[])
-        current_test_size = 0
-        test_index = 0
-        while current_test_size < test_size:
-            rewards = self.data["rewards"][test_index]
-            starts, ends, returns = self.trajectory2pairs(rewards)
-            self.test_data["observations"].append(self.data["observations"][test_index])
-            self.test_data["actions"].append(self.data["actions"][test_index])
-            self.test_data["rewards"].append(self.data["rewards"][test_index])
-            self.test_data["starts"].append(starts+len(rewards))
-            self.test_data["ends"].append(ends+len(rewards))
-            self.test_data["returns"].append(returns)
-            test_index += 1
-            current_test_size += returns.shape[0]
-        self.test_data = {k:np.concatenate(v) for k, v in self.test_data.items()}
-        test_source, test_target = self.batching_from_indices(self.test_data["observations"], self.test_data["starts"],
-                                                 self.test_data["ends"])
-        self.batched_test = dict(source=test_source, target=test_target, returns=self.test_data["returns"],
-                                 distance=discount**(self.test_data["ends"]-self.test_data["starts"]))
-        self.train_raw = {k: v[test_index:] for k, v in self.data.items()}
-        self.nb_train_trajectories = len(self.train_raw["rewards"])
+        with torch.no_grad():
+            self.test_data = dict(observations=[], actions=[], rewards=[], starts=[], ends=[], returns=[])
+            current_test_size = 0
+            test_index = 0
+            while current_test_size < test_size:
+                rewards = self.data["rewards"][test_index]
+                starts, ends, returns = self.trajectory2pairs(rewards)
+                self.test_data["observations"].append(self.data["observations"][test_index])
+                self.test_data["actions"].append(self.data["actions"][test_index])
+                self.test_data["rewards"].append(self.data["rewards"][test_index])
+                self.test_data["starts"].append(starts+len(rewards))
+                self.test_data["ends"].append(ends+len(rewards))
+                self.test_data["returns"].append(returns)
+                test_index += 1
+                current_test_size += returns.shape[0]
+            self.test_data = {k:torch.cat(v) for k, v in self.test_data.items()}
+            test_source, test_target = self.batching_from_indices(self.test_data["observations"], self.test_data["starts"],
+                                                     self.test_data["ends"])
+            self.batched_test = dict(source=test_source, target=test_target, returns=self.test_data["returns"],
+                                     distance=discount**(self.test_data["ends"]-self.test_data["starts"]))
+            self.train_raw = {k: v[test_index:] for k, v in self.data.items()}
+            self.nb_train_trajectories = len(self.train_raw["rewards"])
 
+    @torch.no_grad()
     def sample_minibatch(self, nb_trajectories, pairs_per_trajectory):
-        trajectory_idx = np.random.randint(self.nb_train_trajectories, size=nb_trajectories)
+        trajectory_idx = torch.randint(self.nb_train_trajectories, size=(nb_trajectories,),
+                                       device=self.train_raw["rewards"][0].device)
         observations = [self.train_raw["observations"][i] for i in trajectory_idx]
         rewards = [self.train_raw["rewards"][i] for i in trajectory_idx]
 
@@ -105,19 +114,19 @@ class TunnelDataSet():
         batched_distance = []
         for observation, reward in zip(observations, rewards):
             length = observation.shape[0]
-            rand_idx = np.random.randint(length, size=[pairs_per_trajectory, 2])
-            start_idx = rand_idx.min(axis=1)
-            end_idx = rand_idx.max(axis=1)
+            rand_idx = torch.randint(length, size=(pairs_per_trajectory, 2), device=reward.device)
+            start_idx = rand_idx.min(dim=1).values
+            end_idx = rand_idx.max(dim=1).values
 
             batched_source.append(observation[start_idx])
             batched_target.append(observation[end_idx])
-            batched_returns.append(get_returns(start_idx, end_idx, reward))
+            batched_returns.append(get_returns(start_idx, end_idx, reward, self.discount))
             batched_distance.append(self.discount**(end_idx-start_idx))
 
-        batched_source = np.concatenate(batched_source, axis=0)
-        batched_target = np.concatenate(batched_target, axis=0)
-        batched_returns = np.concatenate(batched_returns, axis=0)
-        batched_distance = np.concatenate(batched_distance, axis=0)
+        batched_source = torch.cat(batched_source, dim=0)
+        batched_target = torch.cat(batched_target, dim=0)
+        batched_returns = torch.cat(batched_returns, dim=0)
+        batched_distance = torch.cat(batched_distance, dim=0)
         return dict(source=batched_source, target=batched_target, returns=batched_returns, distance=batched_distance)
 
     def batching_from_indices(self, observations, starts, ends):
@@ -130,10 +139,10 @@ class TunnelDataSet():
         start_indices = []
         end_indices = []
         for start in range(length):
-            start_indices.append(np.ones(length-start, dtype=np.int32)*start)
-            end_indices.append(np.arange(start, length))
-        starts, ends = np.concatenate(start_indices), np.concatenate(end_indices)
-        returns = get_returns(starts, ends, rewards)
+            start_indices.append(torch.ones(length-start, dtype=torch.int64, device=rewards.device)*start)
+            end_indices.append(torch.arange(start, length, dtype=torch.int64, device=rewards.device))
+        starts, ends = torch.cat(start_indices), torch.cat(end_indices)
+        returns = get_returns(starts, ends, rewards, self.discount)
         return starts, ends, returns
 
 
@@ -163,13 +172,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Experiment
     parser.add_argument("--env", default="hopper-medium-v0")  # OpenAI gym environment name
+    parser.add_argument("--id", default="T-1-1")
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument("--discount", default=0.99, type=int)
     # parser.add_argument("--distance_head", default="discount", type=str)
 
     parser.add_argument("--batch_size", default=20480, type=int)
-    parser.add_argument("--pairs", default=40, type=int)
-    parser.add_argument("--iterations", default=1e5, type=int)
+    parser.add_argument("--pairs", default=512, type=int)
+    parser.add_argument("--iterations", default=1e6, type=int)
     parser.add_argument("--lr", default=0.0001, type=int)
 
     parser.add_argument("--log_period", default=1000, type=int)
@@ -183,19 +193,25 @@ if __name__ == "__main__":
     args = parser.parse_args()
     env = gym.make(args.env)
 
+    results_dir = os.path.expandvars(os.path.expanduser(os.path.join('~/locallogs/tunnel', args.id)))
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+
     # Set seeds
     env.seed(args.seed)
     env.action_space.seed(args.seed)
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
-    raw_dataset = env.get_dataset()
+    raw_dataset = dict_to_tensor(env.get_dataset(), device=device)
     dataset = TunnelDataSet(divide2trajectories(raw_dataset), args.test_size, args.discount)
     model = TunnelModel(raw_dataset["observations"].shape[-1])
     model.to(device=device)
 
     loss_f = nn.MSELoss(reduction="mean")
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    record = dict(step=[], return_loss=[], distance_loss=[], test_return_error=[], test_distance_error=[])
 
     for step in range(int(args.iterations)):
         """
@@ -205,7 +221,7 @@ if __name__ == "__main__":
                                                       dataset.discount,
                                                       args.batch_size//args.pairs, args.pairs), device=device)
         """
-        minibatch = dict_to_tensor(dataset.sample_minibatch(args.batch_size//args.pairs, args.pairs), device=device)
+        minibatch = dataset.sample_minibatch(args.batch_size//args.pairs, args.pairs)
         returns_pred, distance_pred = model(minibatch["source"], minibatch["target"])
         return_loss = loss_f(returns_pred, minibatch["returns"])
         distance_loss = loss_f(distance_pred, minibatch["distance"])
@@ -216,10 +232,22 @@ if __name__ == "__main__":
 
         if step % args.log_period == 0:
             with torch.no_grad():
-                test_data = dict_to_tensor(dataset.batched_test, device=device)
+                test_data = dataset.batched_test
                 returns_pred, distance_pred = model(test_data["source"], test_data["target"])
                 return_error = torch.mean(torch.abs(returns_pred-test_data["returns"]))
                 distance_error = torch.mean(torch.abs(distance_pred-test_data["distance"]))
 
+            record["step"].append(step)
+            record["return_loss"].append(return_loss.item())
+            record["distance_loss"].append(distance_loss.item())
+            record["test_return_error"].append(return_error.item())
+            record["test_distance_error"].append(distance_error.item())
+            pd.DataFrame(record).to_csv(f"{results_dir}/model_training_logs.csv")
+            torch.save({
+                'step': step,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss,
+            }, results_dir+"/model.ckpt")
             print(f"iteration {step}, return loss {return_loss.item()}, distance_loss {distance_loss.item()},"
                   f"test return error {return_error.item()}, test distance error {distance_error.item()}")
